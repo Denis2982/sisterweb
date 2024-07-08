6f199c4c5795391d2263557345ac6c8b4aef76f4
Denis2982 <denisdenis2982@gmail.com>
2024-07-08 16:36:34 +0300

    add content

--- js/script-wish.js
+++ js/script-wish.js
@@ -5,9 +5,13 @@ const btnClose = document.querySelector('.close')
 const popUp = document.querySelector('.pop-up')
 const cards = document.querySelectorAll('.wish__card')
 const video = document.querySelector('.pop-up video')
+const audioWish = document.querySelector('.wish_audio')
+
+audioWish.volume = 0.5
 
 cards.forEach(card => {
 	card.onclick = () => {
+		audioWish.volume = 0.04;
 		popUp.classList.add('open')
 		video.src = `./img/wish/${card.id}.mp4`
 		video.play()
@@ -17,4 +21,7 @@ cards.forEach(card => {
 btnClose.onclick = () => {
 	video.pause()
 	popUp.classList.remove('open')
+	audioWish.volume = 0.5;
 }
+
+
